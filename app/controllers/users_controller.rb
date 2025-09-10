875a0aa9574697b952872c0ee0f563ae096bcc84
yasir <mohammadyasirkmt@gmail.com>
2025-09-10 21:39:41 +0530

    learned about validations where covered where to write validations and how does validations triggered. applied validation using validates method also saw presence, inclusion, exclusion, format, lenght, uniqueness and numericality, writes custom validations and last how to show validation errors on client side using errors and full_messages methods.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to action: :list_users
     else
-      render :new_user
+      render :new_user, status: :unprocessable_entity
     end
 
 
@@ -45,6 +45,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :age, :city_id, :gender)
+    params.require(:user).permit(:first_name, :last_name, :age, :city_id, :gender, :cityname, :country)
   end
 end
