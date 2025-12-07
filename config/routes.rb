1986d961a8488dc8dbe3910c2277a11a2688f6da
yasir <mohammadyasirkmt@gmail.com>
2025-12-07 21:58:42 +0530

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get "/about", to: "static_pages#about", as: "about"
   get "/help", to: "static_pages#help", as: "help"
   get "/contact", to: "static_pages#contact", as: "contact"
-  get '/signup', to: "users#new", as: "signup"
+  get "/signup", to: "users#new", as: "signup"
+  resources :users
 end
