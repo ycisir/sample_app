0fe61d13974053294e9d5a32e02094b019a229dc
yasir <mohammadyasirkmt@gmail.com>
2026-01-07 15:35:05 +0530

    reconfigure mail delivery method

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
 
 	# Sends activation email.
 	def send_activation_email
-		UserMailer.account_activation(self).deliver_later
+		UserMailer.account_activation(self).deliver_now
 	end
 
 	# Sets the password reset attributes.
