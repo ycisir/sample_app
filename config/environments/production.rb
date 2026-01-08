cb133953a499a379ef5e1c862a087c11f2311fad
yasir <mohammadyasirkmt@gmail.com>
2026-01-08 07:48:11 +0530

    fix production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   host = 'sample-app-ysyg.onrender.com'
   config.action_mailer.default_url_options = { host: host }
